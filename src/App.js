afb0f36c1b776346b4306fa6f0254b98ee6d7a04
codecaiine <akabrouyannickn@gmail.com>
2021-10-05 17:21:02 +0000

    Add React-Redux Provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import './App.css';
+import Navbar from './components/Navbar';
+import { Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Navbar from './components/Navbar';
+import store from './redux/configureStore';
+import AllBooks from './components/AllBooks';
+import Categories from './components/Categories';
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello world
-        </p>
-      </header>
-    </div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <AllBooks />
+          </Route>
+          <Route path="/categories">
+            <Categories />
+          </Route>
+        </Switch>
+      </div>
+      </Provider>
   );
 }
 
